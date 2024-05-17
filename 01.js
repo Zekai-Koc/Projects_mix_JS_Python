25ca30e6d4998c210bc2b56a52d91b3f89214425
Zekai Koç <zekaikoc@gmail.com>
2024-05-17 09:52:33 +0200

    another method.

--- 01.js
+++ 01.js
@@ -6,18 +6,34 @@
 // const area = calculateCircleArea(radius);
 // console.log("The area of the circle is:", area);
 
-const readline = require("readline");
-const rl = readline.createInterface({
-   input: process.stdin,
-   output: process.stdout,
-});
+//
+
+// const readline = require("readline");
+// const rl = readline.createInterface({
+//    input: process.stdin,
+//    output: process.stdout,
+// });
+
+// function calculateCircleArea(radius) {
+//    return Math.PI * radius ** 2;
+// }
+
+// rl.question("Enter the radius of the circle: ", function (radius) {
+//    const area = calculateCircleArea(parseFloat(radius));
+//    console.log("The area of the circle is:", area);
+//    rl.close();
+// });
+
+//
 
 function calculateCircleArea(radius) {
    return Math.PI * radius ** 2;
 }
 
-rl.question("Enter the radius of the circle: ", function (radius) {
-   const area = calculateCircleArea(parseFloat(radius));
-   console.log("The area of the circle is:", area);
-   rl.close();
-});
+function main() {
+   const radius = 5.0;
+   const area = calculateCircleArea(radius);
+   console.log(`The area of the circle with radius ${radius} is: ${area}`);
+}
+
+main();
