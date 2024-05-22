d3967302922450d04e45da9396cc7a7aed63e82d
Zekai Koç <zekaikoc@gmail.com>
2024-05-22 10:53:23 +0200

    another method.

--- calculateRectangleArea.py
+++ calculateRectangleArea.py
@@ -1,12 +1,30 @@
-def calculate_area(length, width):
-    if length <= 0 or width <= 0:
-        raise ValueError("Length and width must be positive numbers.")
-    return length * width
+# def calculate_area(length, width):
+#     if length <= 0 or width <= 0:
+#         raise ValueError("Length and width must be positive numbers.")
+#     return length * width
 
-length = 5
-width = 3
+# length = 5
+# width = 3
+# try:
+#     area = calculate_area(length, width)
+#     print(f"The area of the rectangle is: {area}")
+# except ValueError as e:
+#     print(e)
+
+
+class Rectangle:
+    def __init__(self, length, width):
+        if length <= 0 or width <= 0:
+            raise ValueError("Length and width must be positive numbers.")
+        self.length = length
+        self.width = width
+
+    def calculate_area(self):
+        return self.length * self.width
+
 try:
-    area = calculate_area(length, width)
+    rectangle = Rectangle(5, 3)
+    area = rectangle.calculate_area()
     print(f"The area of the rectangle is: {area}")
 except ValueError as e:
     print(e)
