0dac4c9a27453814987371300d7b2d2b28ff2d82
Zekai Koç <zekaikoc@gmail.com>
2024-05-27 11:58:38 +0200

    new method.

--- calculateSphereVolume.js
+++ calculateSphereVolume.js
@@ -7,19 +7,33 @@
 // var volume = calculateSphereVolume(radius);
 // console.log("The volume of the sphere is: " + volume);
 
-function power(base, exponent) {
-   let result = 1;
-   for (let i = 0; i < exponent; i++) {
-      result *= base;
+// function power(base, exponent) {
+//    let result = 1;
+//    for (let i = 0; i < exponent; i++) {
+//       result *= base;
+//    }
+//    return result;
+// }
+
+// function calculateSphereVolume(radius) {
+//    var volume = (4 / 3) * Math.PI * power(radius, 3);
+//    return volume;
+// }
+
+// var radius = parseFloat(prompt("Enter the radius of the sphere:"));
+// var volume = calculateSphereVolume(radius);
+// console.log("The volume of the sphere is: " + volume);
+
+class Sphere {
+   constructor(radius) {
+      this.radius = radius;
    }
-   return result;
+
+   volume() {
+      return (4 / 3) * Math.PI * Math.pow(this.radius, 3);
+   }
 }
 
-function calculateSphereVolume(radius) {
-   var volume = (4 / 3) * Math.PI * power(radius, 3);
-   return volume;
-}
-
-var radius = parseFloat(prompt("Enter the radius of the sphere:"));
-var volume = calculateSphereVolume(radius);
-console.log("The volume of the sphere is: " + volume);
+const radius = parseFloat(prompt("Enter the radius of the sphere:"));
+const sphere = new Sphere(radius);
+console.log("The volume of the sphere is: " + sphere.volume());
