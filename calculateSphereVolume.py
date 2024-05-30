e68f1fde1587e3410e70a1f72826a041b8828828
Zekai Koç <zekaikoc@gmail.com>
2024-05-30 14:49:21 +0200

    new method.

--- calculateSphereVolume.py
+++ calculateSphereVolume.py
@@ -24,13 +24,30 @@
 # print("The volume of the sphere is:", volume)
 
 
-from sympy import symbols, pi
+# from sympy import symbols, pi
+
+# def calculate_sphere_volume(radius):
+#     r = symbols('r')
+#     volume_expr = (4/3) * pi * r**3
+#     volume = volume_expr.subs(r, radius)
+#     return volume.evalf()
+
+# radius = float(input("Enter the radius of the sphere: "))
+# volume = calculate_sphere_volume(radius)
+# print("The volume of the sphere is:", volume)
+
+
+def power(base, exponent):
+    if exponent == 0:
+        return 1
+    elif exponent == 1:
+        return base
+    else:
+        return base * power(base, exponent - 1)
 
 def calculate_sphere_volume(radius):
-    r = symbols('r')
-    volume_expr = (4/3) * pi * r**3
-    volume = volume_expr.subs(r, radius)
-    return volume.evalf()
+    volume = (4/3) * 3.141592653589793 * power(radius, 3)
+    return volume
 
 radius = float(input("Enter the radius of the sphere: "))
 volume = calculate_sphere_volume(radius)
