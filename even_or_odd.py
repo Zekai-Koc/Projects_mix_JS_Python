00a63997a8c8c9af9194035075147ee1589443ce
Zekai Koç <zekaikoc@gmail.com>
2024-05-31 11:23:57 +0200

    new challenge.

---
+++ even_or_odd.py
@@ -0,0 +1,9 @@
+def check_even_or_odd(number):
+    if number % 2 == 0:
+        print(f"{number} is even.")
+    else:
+        print(f"{number} is odd.")
+
+# Example usage:
+number = 4
+check_even_or_odd(number)
