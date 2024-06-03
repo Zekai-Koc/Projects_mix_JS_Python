edd9a5868ecbeb757896b85e5109250247920301
Zekai Koç <zekaikoc@gmail.com>
2024-06-03 13:18:08 +0200

    another method.

--- even_or_odd.js
+++ even_or_odd.js
@@ -10,8 +10,20 @@
 // let number = 4;
 // checkEvenOrOdd(number);
 
+// function checkEvenOrOdd(number) {
+//    if ((number & 1) === 0) {
+//       console.log(number + " is even.");
+//    } else {
+//       console.log(number + " is odd.");
+//    }
+// }
+
+// // Example usage:
+// let number = 4;
+// checkEvenOrOdd(number);
+
 function checkEvenOrOdd(number) {
-   if ((number & 1) === 0) {
+   if (Math.floor(number / 2) * 2 === number) {
       console.log(number + " is even.");
    } else {
       console.log(number + " is odd.");
