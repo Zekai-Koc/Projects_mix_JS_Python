5fa1a93ffd80e8b10f4d3c2e5fa95be026fbf6e4
Zekai Koç <zekaikoc@gmail.com>
2024-06-03 13:17:36 +0200

    another method.

--- even_or_odd.py
+++ even_or_odd.py
@@ -8,8 +8,18 @@
 # number = 4
 # check_even_or_odd(number)
 
+# def check_even_or_odd(number):
+#     if number & 1 == 0:
+#         print(f"{number} is even.")
+#     else:
+#         print(f"{number} is odd.")
+
+# # Example usage:
+# number = 4
+# check_even_or_odd(number)
+
 def check_even_or_odd(number):
-    if number & 1 == 0:
+    if (number // 2) * 2 == number:
         print(f"{number} is even.")
     else:
         print(f"{number} is odd.")
